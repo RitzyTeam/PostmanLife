ebcac0df4d0155c88a8af0a9c205fb8c75b0ef2c
DELALEX <tania377033122@gmail.com>
2024-02-20 13:02:31 +0300

    Незначительные исправления

--- objects/MOBS/ghost_girl/ghost_girl.gd
+++ objects/MOBS/ghost_girl/ghost_girl.gd
@@ -1,1 +1,16 @@
 extends CharacterBody3D
+
+@onready var mesh = $MeshInstance3D
+
+
+
+func _on_appearance_trigger_body_entered(body):
+	if body is CharacterBody3D and 'inv' in body and body.has_method('handle_movement'):
+		# if it is player
+		mesh.visible = true
+
+
+func _on_appearance_trigger_body_exited(body):
+	if body is CharacterBody3D and 'inv' in body and body.has_method('handle_movement'):
+		# if it is player
+		mesh.visible = false
