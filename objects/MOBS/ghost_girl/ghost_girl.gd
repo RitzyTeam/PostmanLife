979a5f2ea2df0ff51286b05e12bf58d2d7c85a96
DELALEX <tania377033122@gmail.com>
2024-02-22 11:42:25 +0300

    Девочка поворачивается

--- objects/MOBS/ghost_girl/ghost_girl.gd
+++ objects/MOBS/ghost_girl/ghost_girl.gd
@@ -1,16 +1,27 @@
 extends CharacterBody3D
 
 @onready var mesh = $mesh
+var isTargettingPlayer: bool = false
+var obj_targetting: Object = null
 
-
+func _physics_process(delta):
+	if isTargettingPlayer:
+		look_at(Vector3(obj_targetting.global_position.x, global_position.y, obj_targetting.global_position.z), Vector3.UP)
+		
 
 func _on_appearance_trigger_body_entered(body):
 	if body is CharacterBody3D and 'inv' in body and body.has_method('handle_movement'):
 		# if it is player
+		isTargettingPlayer = true
 		mesh.visible = true
+		obj_targetting = body
+		$AnimationPlayer.play('show')
 
 
 func _on_appearance_trigger_body_exited(body):
 	if body is CharacterBody3D and 'inv' in body and body.has_method('handle_movement'):
 		# if it is player
+		isTargettingPlayer = false
 		mesh.visible = false
+		obj_targetting = null
+		$AnimationPlayer.play('hide')
