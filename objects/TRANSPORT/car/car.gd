753e048b817447b3ad2699b8c99f784b486d7bea
DELALEX <tania377033122@gmail.com>
2024-02-26 19:40:45 +0300

    Машина (Это просто имбуха)
    Пока что не заправляемая, но вот сейчас начну делать заправку.
    Максимально детальная проработка машины со всеми ее приколами

--- objects/TRANSPORT/car/car.gd
+++ objects/TRANSPORT/car/car.gd
@@ -2,6 +2,7 @@ extends VehicleBody3D
 
 @export var player: PackedScene
 
+
 @onready var wheel_1 = $wheel_1
 @onready var wheel_2 = $wheel_2
 @onready var wheel_3 = $wheel_3
@@ -10,18 +11,29 @@ extends VehicleBody3D
 @onready var rul = $rul_hinge/rul
 @onready var fara_1 = $fara_1
 @onready var fara_2 = $fara_2
+@onready var handle = $automat/handle
 
+# CAR SYSTEMS
+var isFlashlightsOn: bool = false
+# PLAYER-RELATED
 var isPlayerInside: bool = false
+# RESOURCES
+var res_fuel: float = 100.0
+var res_energy: float = 100.0
+var consumption_fuel: float = 0.0001 # drive purposes
+var consumption_energy: float = 0.0001 # light purposes
+# RUL ANIMATION
+var steer_points: float = 0.0
+var max_steer_points: float = 720.0
 # WHEEL SETTINGS
 var wheel_stiffness: float = 100.0
 var wheel_max_force: float = 16000.0
 var wheel_travel_distance: float = 0.1
 # ENGINE SETTINGS
 var MAX_STEER: float = 0.4
-var ENGINE_POWER: float = 5000
+var ENGINE_POWER: float = 15000
 
 func _ready():
-	lights_on()
 	# SET STIFFNESS
 	wheel_1.suspension_stiffness = wheel_stiffness
 	wheel_2.suspension_stiffness = wheel_stiffness
@@ -41,20 +53,67 @@ func _ready():
 func _input(event):
 	if isPlayerInside:
 		if event.is_action_pressed('key_f'):
+			if int(res_energy) > 0:
+				isFlashlightsOn = not isFlashlightsOn
+				if isFlashlightsOn:
+					lights_on()
+					$stat_flashlight/icon.texture = load('res://assets/images/res_flashlight_green.png')
+				else:
+					lights_off()
+					$stat_flashlight/icon.texture = load('res://assets/images/res_flashlight.png')
+		if event.is_action_pressed('rmb'):
+			if int(res_energy) > 0:
+				if not $beep.playing:
+					$beep.play()
+					res_energy -= consumption_energy * 1000
+					$stat_energy/value.text = str(int(res_energy)) + '%'
+		if event.is_action_pressed('key_e'):
 			leave()
 
 
-
 func _physics_process(delta):
-	if isPlayerInside:
-		steering = move_toward(steering, Input.get_axis("move_right", "move_left") * MAX_STEER, delta * 2.5)
-		engine_force = Input.get_axis("move_backward", "move_foward") * ENGINE_POWER
-		if Input.is_action_pressed('move_left'):
-			rul.rotation.y = lerp(rul.rotation.y, deg_to_rad(90), 0.05)
-		elif Input.is_action_pressed('move_right'):
-			rul.rotation.y = lerp(rul.rotation.y, deg_to_rad(-90), 0.05)
+	if isFlashlightsOn:
+		res_energy -= consumption_energy * 1000
+		if int(res_energy) > 0:
+			$stat_energy/value.text = str(int(res_energy)) + '%'
 		else:
-			rul.rotation.y = lerp(rul.rotation.y, deg_to_rad(0), 0.05)
+			$stat_energy/value.text = '0%'
+			$stat_flashlight/icon.texture = load('res://assets/images/res_flashlight.png')
+			lights_off()
+	if isPlayerInside:
+		# UPDATE UI
+		var speed: float = abs(int(linear_velocity.x + linear_velocity.z))
+		if int(res_energy) > 0:
+			$speedometer/speed.text = str(speed) + ' км/ч'
+		# ANIMATE LEVER
+		if int(res_energy) > 0:
+			set_lever(speed)
+		# DRIVING FUNCTION
+		if int(res_fuel) > 0:
+			steering = move_toward(steering, Input.get_axis("move_right", "move_left") * MAX_STEER, delta * 2.5)
+			engine_force = Input.get_axis("move_backward", "move_foward") * ENGINE_POWER
+		else:
+			engine_force = 0
+		# CONTROLS
+		if Input.is_action_pressed('move_foward'):
+			res_fuel -= consumption_fuel
+			if int(res_energy) > 0:
+				$stat_fuel/value.text = str(int(res_fuel)) + '%'
+		if Input.is_action_pressed('move_backward'):
+			res_fuel -= consumption_fuel
+			if int(res_energy) > 0:
+				$stat_fuel/value.text = str(int(res_fuel)) + '%'
+		# STEERING BY RUL
+		if Input.is_action_pressed('move_left'):
+			if steer_points < max_steer_points:
+				steer_points += 2.0
+		elif Input.is_action_pressed('move_right'):
+			if steer_points > -max_steer_points:
+				steer_points -= 2.0
+		else:
+			steer_points = lerp(steer_points, 0.0, 0.05)
+		
+		rul.rotation.y = lerp(rul.rotation.y, deg_to_rad(steer_points), 0.05)
 	
 func enter(player_obj: Object):
 	if not isPlayerInside:
@@ -64,6 +123,8 @@ func enter(player_obj: Object):
 
 func leave():
 	if isPlayerInside:
+		steering = 0
+		engine_force = 0
 		isPlayerInside = false
 		var player_obj = player.instantiate()
 		get_tree().get_root().add_child(player_obj)
@@ -77,3 +138,21 @@ func lights_on():
 func lights_off():
 	fara_1.light_energy = 0
 	fara_2.light_energy = 0
+
+func set_lever(speed: float):
+	if speed > 0 and speed < 15:
+		handle.position.z = lerp(handle.position.z,  0.575, 0.1)
+		handle.position.x = lerp(handle.position.x,  -0.332, 0.1)
+	elif speed >= 15 and speed < 30:
+		handle.position.z = lerp(handle.position.z,  0.195, 0.1)
+		handle.position.x = lerp(handle.position.x,  -0.332, 0.1)
+	elif speed >= 30 and speed < 45:
+		handle.position.z = lerp(handle.position.z,  -0.13, 0.1)
+		handle.position.x = lerp(handle.position.x,  -0.332, 0.1)
+	elif speed >= 45 and speed < 60:
+		handle.position.z = lerp(handle.position.z,  -0.455, 0.1)
+		handle.position.x = lerp(handle.position.x,  -0.332, 0.1)
+	elif speed >= 60:
+		handle.position.z = lerp(handle.position.z,  0.37, 0.1)
+		handle.position.x = lerp(handle.position.x,  0.273, 0.1)
+		
