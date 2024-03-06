48704e390e2a327019d95c0362081210c45c2522
DELALEX <tania377033122@gmail.com>
2024-03-06 22:03:21 +0300

    Обработаны фонари. Включаются и выключаются по мировым сигналам

--- objects/PROPS/environment/lampPost.gd
+++ objects/PROPS/environment/lampPost.gd
@@ -1,7 +1,14 @@
 extends Node3D
 
 @onready var spot_light_3d = $SpotLight3D
+@onready var anim_light = $anim_light
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _ready():
+	SIN_WORLD_SIGNALS.LIGHTS_ON.connect(_lights_on)
+	SIN_WORLD_SIGNALS.LIGHTS_OFF.connect(_lights_off)
+
+func _lights_on():
+	anim_light.play("lights_on")
+
+func _lights_off():
+	anim_light.play("lights_off")
--- singletons/SIN_WORLD_SIGNALS.gd
+++ singletons/SIN_WORLD_SIGNALS.gd
@@ -3,3 +3,5 @@ extends Node
 signal PACKAGE_DELIVERED(task_id)
 signal PACKAGE_FAILED(task_id)
 signal PACKAGE_CREATED(task_data)
+signal LIGHTS_ON
+signal LIGHTS_OFF
