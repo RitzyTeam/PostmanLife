7f55d787afffd339a19a4202831b433f789251f4
DELALEX <delalexdev@ya.ru>
2024-03-16 01:30:47 +0300

    Исправление ошибки #18

--- objects/urban_elements/mine/mine.gd
+++ objects/urban_elements/mine/mine.gd
@@ -2,11 +2,12 @@ extends Node3D
 
 
 func blow():
-	get_tree().change_scene_to_file('res://scenes/DEATHS/death_mine/death_mine.tscn')
+	get_tree().change_scene_to_file.bind("res://scenes/DEATHS/death_mine/death_mine.tscn").call_deferred()
 
 
 func _on_trigger_body_exited(body):
-	if body is CharacterBody3D:
+	if body.is_in_group('player'):
+		body.queue_free()
 		blow()
 
 
