7dc6f7e4ff8a6e6f2c8a8d3100578b6854459f52
FingerFrog <perfolcon@gmail.com>
2024-04-04 22:41:18 +0300

    аддон юнит тестов, отчет Саши

---
+++ unitTest/test_gas_buy.gd
@@ -0,0 +1,30 @@
+extends GutTest
+
+var petrol_left: int = 1000
+var petrol_price: int = 1
+# HOW MUCH CAN YOU BUY FOR A ONE CANISTER
+var buy_amount: int = 20
+var player_money: int = 100
+
+func test_assert_eq_number_not_equal():
+	assert_eq(buy_petrol(), true, "Must pass test.")
+
+func buy_petrol():
+	var isSuccess: bool = false
+	var amountBought: int = 0
+	if petrol_left >= buy_amount:
+		var petrol_price: int = buy_amount * petrol_price
+		if player_money >= petrol_price:
+			player_money -= petrol_price
+			isSuccess = true
+			amountBought = buy_amount
+			petrol_left -= buy_amount
+	else:
+		if petrol_left > 0:
+			if player_money >= petrol_price:
+				isSuccess = true
+				amountBought = petrol_left
+				player_money -= petrol_price
+				petrol_left = 0
+	gut.p(isSuccess)
+	return isSuccess
